3765f605087c59d1ed979dbb57f9d557fdee0e65
Logan Lickfield <l.lickfield@gmail.com>
2024-09-27 17:13:11 -0400

    Completed basic calculator logic. Returns answers on one step calculations. Also can chain operations, as long as you hit 'equal' every time.

--- calculator.js
+++ calculator.js
@@ -11,11 +11,54 @@ numbers.forEach((number) => {
     });
 });
 
+let multiply = document.querySelector('#multiply');
+multiply.addEventListener('click', () => {
+    calledFunction.function = 'multiply';
+    calledFunction.firstVariable = display.textContent;
+    display.textContent = '0';
+});
+let divide = document.querySelector('#divide');
+divide.addEventListener('click', () => {
+    calledFunction.function = 'divide';
+    calledFunction.firstVariable = display.textContent;
+    display.textContent = '0';
+});
+let add = document.querySelector('#add');
+add.addEventListener('click', () => {
+    calledFunction.function = 'add';
+    calledFunction.firstVariable = display.textContent;
+    display.textContent = '0';
+});
+let subtract = document.querySelector('#subtract');
+subtract.addEventListener('click', () => {
+    calledFunction.function = 'subtract';
+    calledFunction.firstVariable = display.textContent;
+    display.textContent = '0';
+});
+let evaluate = document.querySelector('#evaluate');
+evaluate.addEventListener('click', () => {
+    calledFunction.secondVariable = display.textContent;
+    display.textContent = functions[calledFunction.function](
+        calledFunction.firstVariable, calledFunction.secondVariable);
+    calledFunction = {
+        function: '',
+        firstVariable: '',
+        secondVariable: '',
+    }
+});
+
+let calledFunction = {
+    function: '',
+    firstVariable: '',
+    secondVariable: '',
+};
 
 let functions = {
-    add: add(),
-    subtract: subtract(),
-    multiply: multiply(),
-    divide: divide(),
-    equal: evaluate(),
+    add: function(a, b) {return parseFloat(a) + parseFloat(b)},
+    subtract: function(a, b) {return a - b},
+    multiply: function(a, b) {return a * b},
+    divide: function(a, b) {return a / b},
+    equal: function(operation, a, b) {
+        operation(a, b);
+    },
 }
