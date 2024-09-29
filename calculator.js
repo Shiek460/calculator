926d89f3894bff08b6eea2e18fc03693f88428be
Logan Lickfield <l.lickfield@gmail.com>
2024-09-29 08:21:07 -0400

    Functions working and styling done to whole. Added event listener to 'clear', 'positive/negative', and 'percent' buttons. 'clear' eliminates only current number without altering variables, then changes to 'CE' to empty entire function object.

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ numbers.forEach((number) => {
     number.addEventListener('click', () => {
         if (display.textContent == '0') {
             display.textContent = number.textContent;
+            clear.textContent = 'C'
         } else {
             display.textContent += number.textContent;
         }
@@ -16,36 +17,72 @@ multiply.addEventListener('click', () => {
     calledFunction.function = 'multiply';
     calledFunction.firstVariable = display.textContent;
     display.textContent = '0';
+    clear.textContent = 'C'
 });
 let divide = document.querySelector('#divide');
 divide.addEventListener('click', () => {
     calledFunction.function = 'divide';
     calledFunction.firstVariable = display.textContent;
     display.textContent = '0';
+    clear.textContent = 'C'
 });
 let add = document.querySelector('#add');
 add.addEventListener('click', () => {
     calledFunction.function = 'add';
     calledFunction.firstVariable = display.textContent;
     display.textContent = '0';
+    clear.textContent = 'C'
 });
 let subtract = document.querySelector('#subtract');
 subtract.addEventListener('click', () => {
     calledFunction.function = 'subtract';
     calledFunction.firstVariable = display.textContent;
     display.textContent = '0';
+    clear.textContent = 'C'
 });
-let evaluate = document.querySelector('#evaluate');
-evaluate.addEventListener('click', () => {
-    calledFunction.secondVariable = display.textContent;
-    display.textContent = functions[calledFunction.function](
-        calledFunction.firstVariable, calledFunction.secondVariable);
-    calledFunction = {
-        function: '',
-        firstVariable: '',
-        secondVariable: '',
+let equal = document.querySelector('#evaluate');
+equal.addEventListener('click', evaluate);
+
+function evaluate() {
+    if (calledFunction.secondVariable == '0' && calledFunction.function == 'divide') {
+        alert("Try Again. Can't divide by zero!");
+    } else {
+        calledFunction.secondVariable = display.textContent;
+        display.textContent = functions[calledFunction.function](
+            calledFunction.firstVariable, calledFunction.secondVariable);
+        calledFunction = {
+            function: '',
+            firstVariable: display.textContent,
+            secondVariable: '',
+        }
+        clear.textContent = 'CE'
+    }
+}
+
+let clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    if (clear.textContent == 'C') {
+        display.textContent = '0';
+        clear.textContent = 'CE'
+    } else {
+        calledFunction = {
+            function: '',
+            firstVariable: '',
+            secondVariable: '',
+        };
+        display.textContent = '0';
     }
 });
+
+let abs = document.querySelector("#abs");
+abs.addEventListener('click', () => {
+    display.textContent *= -1;
+});
+
+let percent = document.querySelector('#percent');
+percent.addEventListener('click', () => {
+    display.textContent *= .01;
+})
 
 let calledFunction = {
     function: '',
@@ -58,7 +95,4 @@ let functions = {
     subtract: function(a, b) {return a - b},
     multiply: function(a, b) {return a * b},
     divide: function(a, b) {return a / b},
-    equal: function(operation, a, b) {
-        operation(a, b);
-    },
 }
